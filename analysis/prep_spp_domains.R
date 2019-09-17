d8b6e9eea197c5e231f332abec8a42430aedabff
Kevin See <Kevin.See@biomark.com>
2019-09-17 12:57:35 -0700

    revised prep_spp_domains to make domain objects match naming conventions

--- analysis/prep_spp_domains.R
+++ analysis/prep_spp_domains.R
@@ -1,7 +1,7 @@
 # Author: Kevin See
 # Purpose: Process species ranges
 # Created: 5/14/2019
-# Last Modified: 6/19/19
+# Last Modified: 9/17/19
 # Notes: 
 # From StreamNet: http://www.streamnet.org/data/interactive-maps-and-gis-data/
 # Bureau of Rec provided updated extents in the Upper Salmon watersheds
@@ -14,7 +14,7 @@ library(tidyverse)
 library(sf)
 
 #-----------------------------------------------------------------
-# set projection
+# set projection we'd like to use consistently
 myCRS = 5070
 
 #-----------------------------------------------------------------
@@ -27,9 +27,9 @@ StmNt = st_read('data/raw/domain/FishDist_AllSpecies.shp') %>%
 
 
 # read in population boundaries (polygons)
-chnkPops = st_read('data/raw/domain/CHNK_SPSU_All.shp') %>%
+chnk_pops = st_read('data/raw/domain/CHNK_SPSU_All.shp') %>%
   st_transform(crs = myCRS)
-sthdPops = st_read('data/raw/domain/STHD_SUWI_All.shp') %>%
+sthd_pops = st_read('data/raw/domain/STHD_SUWI_All.shp') %>%
   st_transform(crs = myCRS) %>%
   filter(grepl('summer', RUN_TIMING))
 
@@ -50,26 +50,26 @@ crb = st_read('/Users/kevin/Dropbox/ISEMP/Data/DesignDocs/WatershedBoundaryLines
         geom = .)
 
 # pull out species domains, clip to crb polygon, and join to population polygons
-chnkDomain = StmNt %>%
+chnk_domain = StmNt %>%
   filter(Species == 'Chinook salmon' &
            Run %in% c('Spring', 'Summer')) %>%
   st_zm() %>%
   st_intersection(crb) %>%
   mutate(Source = 'StreamNet') %>%
-  st_join(chnkPops %>%
+  st_join(chnk_pops %>%
             select(ESU_DPS:NWR_NAME),
           largest = T) %>%
   mutate_at(vars(ESU_DPS:NWR_NAME),
             list(fct_drop)) %>%
   select(Source, StreamName, Species, SciName, UseType, ESU_DPS:NWR_NAME)
 
-sthdDomain = StmNt %>%
+sthd_domain = StmNt %>%
   filter(Species == 'Steelhead' &
            Run == 'Summer') %>%
   st_zm() %>%
   mutate(Source = 'StreamNet') %>%
   st_intersection(crb) %>%
-  st_join(sthdPops %>%
+  st_join(sthd_pops %>%
             select(ESU_DPS:NWR_NAME),
           largest = T) %>%
   mutate_at(vars(ESU_DPS:NWR_NAME),
@@ -95,7 +95,7 @@ uppSalmChnk = st_read('data/raw/domain/UP_Salmon_ChinookExtents_All.shp') %>%
                         'Upper Salmon' = 'Salmon River Upper Mainstem above Redfish Lake')) %>%
   rename(StreamName = GNIS_Name)
 
-p1 = chnkDomain %>%
+p1 = chnk_domain %>%
   filter(MPG == 'Upper Salmon River',
          StreamName != 'Salmon River') %>%
   mutate(Basin = str_remove(NWR_NAME, 'Chinook Salmon \\(Snake River Spring/Summer-run ESU\\) \\- ')) %>%
@@ -122,11 +122,11 @@ ggpubr::ggarrange(plotlist = list(p1, p2),
                   legend = 'bottom')
 
 # drop areas of Upper Salmon MPG from StreamNet and replace with BoR ranges
-chnkDomain %>%
+chnk_domain %>%
   filter(!(MPG == 'Upper Salmon River' & StreamName != 'Salmon River')) %>%
   rbind(uppSalmChnk %>%
           mutate(NWR_NAME = paste('Chinook Salmon (Snake River Spring/Summer-run ESU) -', Basin)) %>%
-          left_join(chnkDomain %>%
+          left_join(chnk_domain %>%
                       filter(MPG == 'Upper Salmon River',
                              StreamName != 'Salmon River') %>%
                       as_tibble %>%
@@ -135,7 +135,7 @@ chnkDomain %>%
                       distinct) %>%
           mutate(UseType = 'Spawning, rearing and migration',
                  Source = 'BoR') %>%
-          select(one_of(names(chnkDomain)))) -> chnkDomain
+          select(one_of(names(chnk_domain)))) -> chnk_domain
 
 
 # Steelhead
@@ -144,13 +144,13 @@ uppSalmSthd = st_read('data/raw/domain/UPsalmon_SteelheadExtent_All.shp') %>%
   st_zm() %>%
   mutate(StreamName = GNIS_Name) %>%
   select(-Basin) %>%
-  st_join(sthdPops %>%
+  st_join(sthd_pops %>%
             filter(MPG == 'Salmon River') %>%
             mutate(Basin = str_remove(NWR_NAME, 'Steelhead \\(Snake River Basin DPS\\) \\- ')) %>%
             select(Basin))
 
 
-p1 = sthdDomain %>%
+p1 = sthd_domain %>%
   filter(MPG == 'Salmon River',
          StreamName != 'Salmon River') %>%
   mutate(Basin = str_remove(NWR_NAME, 'Steelhead \\(Snake River Basin DPS\\) \\- ')) %>%
@@ -177,18 +177,18 @@ ggpubr::ggarrange(plotlist = list(p1, p2),
                   legend = 'bottom')
 
 # drop areas of Upper Salmon DPS from StreamNet and replace with BoR ranges
-sthdDomain %>%
+sthd_domain %>%
   filter(!NWR_NAME %in% paste('Steelhead (Snake River Basin DPS) -', unique(uppSalmSthd$Basin))) %>%
   rbind(uppSalmSthd %>%
           mutate(NWR_NAME = paste('Steelhead (Snake River Basin DPS) -', Basin)) %>%
-          left_join(sthdDomain %>%
+          left_join(sthd_domain %>%
                       filter(NWR_NAME %in% paste('Steelhead (Snake River Basin DPS) -', unique(uppSalmSthd$Basin))) %>%
                       as_tibble %>%
                       select(Species, SciName, ESU_DPS:NWR_NAME) %>%
                       distinct) %>%
           mutate(UseType = 'Spawning, rearing and migration',
                  Source = 'BoR') %>%
-          select(one_of(names(sthdDomain)))) -> sthdDomain
+          select(one_of(names(sthd_domain)))) -> sthd_domain
 
 #-----------------------------------------------------------------
 # Upper Grande Ronde extents, based on ODFW designations
@@ -203,16 +203,16 @@ ugrChnk = st_read('data/raw/domain/CRITFC_Chinook_Target_Frame_April2012Copy.shp
   select(Source, StreamName = FEAT_NAME,
          UseType = critfc_use,
          NWR_NAME) %>%
-  left_join(chnkDomain %>%
+  left_join(chnk_domain %>%
               as_tibble %>%
               filter(NWR_NAME %in% .$NWR_NAME) %>%
               select(Species, SciName, ESU_DPS:NWR_NAME) %>%
               distinct) %>%
-  select(one_of(names(chnkDomain)))
+  select(one_of(names(chnk_domain)))
 
-chnkDomain %>%
+chnk_domain %>%
   filter(! NWR_NAME %in% unique(ugrChnk$NWR_NAME)) %>%
-  rbind(ugrChnk) -> chnkDomain
+  rbind(ugrChnk) -> chnk_domain
 
 # steelhead
 ugrSthd = st_read('data/raw/domain/StS_Grum_Spawning_Universe.shp') %>%
@@ -220,33 +220,33 @@ ugrSthd = st_read('data/raw/domain/StS_Grum_Spawning_Universe.shp') %>%
   rename(StreamName = FEAT_NAME) %>%
   mutate(NWR_NAME = 'Steelhead (Snake River Basin DPS) - Grande Ronde River Upper Mainstem',
          Source = 'ODFW') %>%
-  left_join(sthdDomain %>%
+  left_join(sthd_domain %>%
               as_tibble %>%
               filter(NWR_NAME %in% .$NWR_NAME) %>%
               select(Species, SciName, UseType, ESU_DPS:NWR_NAME) %>%
               distinct) %>%
   mutate(UseType = 'Spawning, rearing and migration') %>%
-  select(one_of(names(sthdDomain)))
+  select(one_of(names(sthd_domain)))
 
-sthdDomain %>%
+sthd_domain %>%
   filter(! NWR_NAME %in% unique(ugrSthd$NWR_NAME)) %>%
-  rbind(ugrSthd) -> sthdDomain
+  rbind(ugrSthd) -> sthd_domain
 
 #-----------------------------------------------------------------
 # Save species extents
 #-----------------------------------------------------------------
 # make available like in a package, by calling "data()"
-use_data(chnkDomain, sthdDomain,
+use_data(chnk_domain, sthd_domain,
          version = 2,
          overwrite = T)
 
 # save as shape files
-st_write(chnkDomain,
-         dsn = 'data/prepped/ChnkDomain.shp',
+st_write(chnk_domain,
+         dsn = 'data/prepped/chnk_domain.shp',
          driver = 'ESRI Shapefile',
          delete_layer = T)
 
-st_write(sthdDomain,
-         dsn = 'data/prepped/SthdDomain.shp',
+st_write(sthd_domain,
+         dsn = 'data/prepped/sthd_domain.shp',
          driver = 'ESRI Shapefile',
          delete_layer = T)
