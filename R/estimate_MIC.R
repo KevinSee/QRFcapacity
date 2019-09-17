5992cea4d7437598d529cc99b33ba24d4d1f98b5
Kevin See <Kevin.See@biomark.com>
2019-09-17 11:29:52 -0700

    added several functions

---
+++ R/estimate_MIC.R
@@ -0,0 +1,45 @@
+#' @title Estimate MIC
+#'
+#' @description Estimate various MIC-related statistics
+#'
+#' @author Kevin See
+#'
+#' @param data data.frame containing response as one column, and various covariates as other columns
+#' @param covars vector of potential covariate names, which match column names in \code{data}
+#' @param response name of response, which matches one column name in \code{data}
+#'
+#' @import dplyr minerva
+#' @return NULL
+#' @export
+
+estimate_MIC = function(data,
+                        covars,
+                        response) {
+  
+  res = minerva::mine(x = data %>%
+               dplyr::select(one_of(covars)),
+             y = data %>%
+               pull(response),
+               # dplyr::mutate(fish_dens = log(fish_dens + 0.01)) %>%
+               # dplyr::pull(fish_dens),
+             use = 'pairwise.complete.obs') %>%
+    as_tibble() %>%
+    mutate(Metric = covars[covars %in% names(data)]) %>%
+    select(Metric, everything()) %>%
+    left_join(data %>%
+                select(one_of(covars)) %>%
+                gather(Metric, value) %>%
+                group_by(Metric) %>%
+                summarise(var = var(value, na.rm = T),
+                          obsCV = sd(value, na.rm = T) / mean(value, na.rm = T),
+                          non_NA = sum(!is.na(value)),
+                          is_NA = sum(is.na(value)),
+                          perc_NA = round(is_NA / (is_NA + non_NA), 3),
+                          non_0 = sum(value != 0, na.rm = T),
+                          perc_non0 = non_0 / n()),
+              by = 'Metric') %>%
+    ungroup() %>%
+    select(Metric, var:perc_non0, everything())
+  
+  return(res)
+}
