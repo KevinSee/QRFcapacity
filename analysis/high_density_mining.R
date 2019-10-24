62d7d4ad538b8ece8d1dc79e288cf760dff787c8
Mike Ackerman <mike.ackerman@biomark.com>
2019-10-24 11:40:06 -0600

    initiated high_density_mining.R script

---
+++ analysis/high_density_mining.R
@@ -0,0 +1,18 @@
+# Author: Kevin See
+# Purpose: Examine abundance/densities and explore preferred or target habitat conditions in identified high density areas
+# Created: 10/24/2019
+# Last Modified: 10/24/2019
+# Notes: 
+
+#-----------------------------------------------------------------
+# load needed libraries
+library(QRFcapacity)
+
+#-----------------------------------------------------------------
+# load data
+#-----------------------------------------------------------------
+data("fh_sum_champ_2017")   # summer juvenile parr paired fish/CHaMP habitat data through 2017
+data("fh_win_champ_2017")   # winter juvenile presmolts paired fish/CHaMP habitat data through 2017
+data("fh_redds_champ_2017") # paired redd/CHaMP habitat data through 2017
+
+
