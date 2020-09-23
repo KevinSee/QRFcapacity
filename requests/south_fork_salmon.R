44f5bb1abca3a48903faf0538e79a5acf38ec0f2
Kevin See <Kevin.See@merck.com>
2020-09-23 13:58:06 -0700

    added Secesh to the South Fork Salmon population

--- requests/south_fork_salmon.R
+++ requests/south_fork_salmon.R
@@ -21,7 +21,8 @@ rch_200_cap = st_read(paste0('output/gpkg/Rch_Cap_', mod_choice, '.gpkg'))
 
 # clip out south fork salmon, by Ryan's request
 sf_salm = rch_200_cap %>%
-  filter(grepl("South Fork Salmon River", sthd_NWR_NAME))
+  filter(grepl("South Fork Salmon River", sthd_NWR_NAME) |
+           grepl("Secesh", sthd_NWR_NAME))
 
 sf_salm %>%
   select(chnk_NWR_NAME) %>%
@@ -30,11 +31,13 @@ sf_salm %>%
 #save as shapefile
 st_write(sf_salm,
          dsn = paste0('output/shapefiles/Rch_Cap_', mod_choice, '_South_Fork_Salmon.shp'),
-         driver = 'ESRI Shapefile')
+         driver = 'ESRI Shapefile',
+         append = F)
 
 # save as geopackage
 st_write(sf_salm,
          dsn = paste0('output/shapefiles/Rch_Cap_', mod_choice, '_South_Fork_Salmon.gpkg'),
-         driver = 'GPKG')
+         driver = 'GPKG',
+         append = F)
 
 names(sf_salm)
